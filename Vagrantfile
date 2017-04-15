d2e8e203e9e87b2f05d21bc657c7f53283ac9c62
Brayden Neale <brayden.neale@gmail.com>
2017-04-15 12:22:20 -0400

    Vagrant file configuration and provisioning - Docker, tools ...

--- Vagrantfile
+++ Vagrantfile
@@ -3,15 +3,16 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
-  config.vm.network "public_network"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.define :ubuntu_xenial
 
   config.vm.provision "shell" do |s|
     s.path = "provision.sh"
   end
 
   config.vm.provider "virtualbox" do |v|
-    v.name = "ubuntu_dev"
+    v.name = "ubuntu_16.04"
     v.memory = 2048
-    v.gui = true
+    # v.gui = true
   end
 end
--- provision.sh
+++ provision.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
 sudo apt-get update
+
+#General
+sudo apt-get install -y build-essential curl wget git vim openssl
+
+# # Virtualbox GUI
+# sudo apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+# sudo VBoxClient --clipboard --draganddrop --seamless --display --checkhostversion
+
+# # Any user can launch the GUI
+# sudo sed -ie 's/allowed_users=.*/allowed_users=anybody/' /etc/X11/Xwrapper.config
+
+# Docker
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
+sudo apt-cache policy docker-engine
+sudo apt-get install -y docker-engine
+
+curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# AWS CLI
+sudo apt-get install -y python-pip
+pip install --upgrade pip
+pip install --upgrade --user awscli
